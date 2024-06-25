dc6a47d06ed85d0c1df0197ee9577fe9c08495ef
Huai-Dan Chang <hchang256@wisc.edu>
2024-06-25 02:30:15 -0500

    Initial commit

---
+++ LittleLemonDinnerMenu/LittleLemonDinnerMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonDinnerMenuApp.swift
+//  LittleLemonDinnerMenu
+//
+//  Created by dana chang on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonDinnerMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
